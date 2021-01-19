6e09ac42b36cba3534ce54c724e381354fbb0868
Alex <alexmogwaiii@gmail.com>
2021-01-19 18:13:27 +0200

    Updates

---
+++ precache-manifest.de43e665f5362e057a819f56a452b4a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1c3d5136908041e887dbf9c7df855d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a533607bda21640b0540",
+    "url": "./static/css/main.9af712bb.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a533607bda21640b0540",
+    "url": "./static/js/main.aae8affd.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
